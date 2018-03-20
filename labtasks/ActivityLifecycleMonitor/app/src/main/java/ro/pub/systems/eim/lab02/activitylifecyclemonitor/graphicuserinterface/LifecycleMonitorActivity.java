dc541c31d85f33b0364caba9795acb3df0009783
alexandradinca1995@gmail.com <alexandradinca1995@gmail.com>
2018-03-21 00:14:46 +0200

    solution for lab2

--- labtasks/ActivityLifecycleMonitor/app/src/main/java/ro/pub/systems/eim/lab02/activitylifecyclemonitor/graphicuserinterface/LifecycleMonitorActivity.java
+++ labtasks/ActivityLifecycleMonitor/app/src/main/java/ro/pub/systems/eim/lab02/activitylifecyclemonitor/graphicuserinterface/LifecycleMonitorActivity.java
@@ -7,12 +7,15 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.PopupWindow;
 
 import ro.pub.systems.eim.lab02.activitylifecyclemonitor.R;
 import ro.pub.systems.eim.lab02.activitylifecyclemonitor.general.Constants;
 import ro.pub.systems.eim.lab02.activitylifecyclemonitor.general.Utilities;
+
+import static ro.pub.systems.eim.lab02.activitylifecyclemonitor.general.Constants.USERNAME_EDIT_TEXT;
 
 public class LifecycleMonitorActivity extends AppCompatActivity {
 
@@ -61,8 +64,67 @@ public class LifecycleMonitorActivity extends AppCompatActivity {
         okButton.setOnClickListener(buttonClickListener);
         Button cancelButton = (Button) findViewById(R.id.cancel_button);
         cancelButton.setOnClickListener(buttonClickListener);
-
-        Log.d(Constants.TAG, "onCreate() method was invoked without a previous state");
+        if ((savedInstanceState != null))
+          Log.d(Constants.TAG, "onCreate() method was invoked with a previous state");
+        else
+         Log.d(Constants.TAG, "onCreate() method was invoked without a previous state");
     }
 
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.println(Log.DEBUG, "activitylifecyclemonit", "onRestart method");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.println(Log.DEBUG, "activitylifecyclemonit", "onRestart method");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.println(Log.DEBUG, "activitylifecyclemonit", "onStart method");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.println(Log.DEBUG, "activitylifecyclemonit", "onResume method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.println(Log.DEBUG, "activitylifecyclemonit", "onStop method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.println(Log.DEBUG, "activitylifecyclemonit", "onPause method");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState){
+        super.onSaveInstanceState(saveInstanceState);
+
+        CheckBox rememberMe = (CheckBox)findViewById(R.id.remember_me_checkbox);
+        EditText username = (EditText)findViewById(R.id.username_edit_text);
+        if(rememberMe.isChecked())
+        {
+            saveInstanceState.putString(Constants.USERNAME_EDIT_TEXT, username.getText().toString());
+        }
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        // Always call the superclass so it can restore the view hierarchy
+        super.onRestoreInstanceState(savedInstanceState);
+
+        // Restore state members from saved instance
+        EditText username = (EditText)findViewById(R.id.username_edit_text);
+        username.setText(savedInstanceState.getString(USERNAME_EDIT_TEXT));
+    }
 }
